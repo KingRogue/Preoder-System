ca5fb476be36b9de4bdebad77e20a36fe79ae836
Tim Gebert <kingroguemc@gmail.com>
2017-02-19 12:38:39 +1100

    More

---
+++ src/com/kingrogue/preorder/model/DataController.java
@@ -0,0 +1,14 @@
+package com.kingrogue.preorder.model;
+
+import java.util.List;
+
+/**
+ * Created by Tim G on 18-Feb-17.
+ */
+public class DataController {
+
+    private List<Product> products;
+    private List<Order> orders;
+    private List<Customer> customers;
+    private List<Activity> activities;
+}
